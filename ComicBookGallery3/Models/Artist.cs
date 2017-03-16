9701ed6477f8520dcd3b61b0339d425a5d4b5040
Brian Lau <blau08@gmail.com>
2017-03-15 22:55:06 -0700

    Added Artist and Comic Book Model

---
+++ ComicBookGallery3/Models/Artist.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ComicBookGallery3.Models
+{
+    public class Artist
+    {
+        public string Name { get; set; }
+        public string Role { get; set; }
+    }
+}
